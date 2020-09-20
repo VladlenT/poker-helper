e54d29a63e4c009c1812ef3a72bf61fe88feace5
Vladlen Tereshchenko <vladlent.dev@gmail.com>
2020-09-20 10:46:00 +0300

    feat(rng): added number change animation

--- src/app/components/rng/rng.component.ts
+++ src/app/components/rng/rng.component.ts
@@ -1,13 +1,24 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import random from 'lodash/random';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-rng',
   templateUrl: './rng.component.html',
   styleUrls: ['./rng.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [
+    trigger('numberChange', [
+      transition('* => *', [
+        style({ opacity: '0' }),
+        animate('300ms ease-out', style({ opacity: '*' })),
+      ]),
+    ]),
+  ],
 })
 export class RngComponent implements OnInit {
+  /** Saving timestamp to trigger animation even if new random number equals to the previous */
+  timestamp: number;
   randomNumber: number;
 
   constructor() {}
@@ -16,5 +27,6 @@ export class RngComponent implements OnInit {
 
   generate() {
     this.randomNumber = random(1, 100);
+    this.timestamp = new Date().getTime();
   }
 }
