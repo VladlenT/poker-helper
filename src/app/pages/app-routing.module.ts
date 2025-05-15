de73b34eef42437914b4468aaccd0b5d5f724aae
Vladlen Tereshchenko <vladlent.dev@gmail.com>
2025-05-14 22:32:33 -0400

    updates routes to specify 'cash' for pre-flop ranges

--- src/app/pages/app-routing.module.ts
+++ src/app/pages/app-routing.module.ts
@@ -1,21 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { PreFlopPageComponent } from './pre-flop-page/pre-flop-page.component';
+import { PreFlopPageComponent as PreFlopCachePageComponent } from './pre-flop-page/pre-flop-page.component';
 import { SharedModule } from '../shared/shared.module';
 import { MathPageComponent } from './math-page/math-page.component';
 import { MathPageModule } from './math-page/math-page.module';
 import { ReferencePageComponent } from './reference-page/reference-page.component';
 
 export const pages: Routes = [
-  { path: 'pre-flop', component: PreFlopPageComponent, data: { title: 'Pre-Flop Ranges (Cash)' } },
+  { path: 'pre-flop-cache', component: PreFlopCachePageComponent, data: { title: 'Pre-Flop Ranges (Cash)' } },
   { path: 'math', component: MathPageComponent, data: { title: 'Math' } },
   { path: 'reference', component: ReferencePageComponent, data: { title: 'Reference' } },
 ];
 const routes: Routes = [{ path: '', pathMatch: 'full', redirectTo: pages[0].path }, ...pages];
 
 @NgModule({
-  declarations: [PreFlopPageComponent, ReferencePageComponent],
+  declarations: [PreFlopCachePageComponent, ReferencePageComponent],
   imports: [RouterModule.forRoot(routes), SharedModule, MathPageModule],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
